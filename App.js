78bde4e8c4bad68e96b9dc655388073cd24faf1e
Ivan Gorojovsky <ruso@MacBook-Pro-de-Ivan.local>
2023-01-20 15:03:30 -0300

    ui

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Switch, Text, View, } from 'react-native';
+import { useColorScheme } from 'nativewind';
+import ProductsList from './components/ProductsList';
 
 export default function App() {
+  const { ColorScheme, toggleColorScheme } = useColorScheme();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView className="flex-1 items-center justify-center bg-gray-200 dark:bg-black">
+    <View className="flex-row w-full gap-5">
+      <Text className="text-2xl font-bold dark:text-white">
+        New Collection
+      </Text>
+      <Switch value={ColorScheme === "dark"} onChange={toggleColorScheme} />
+      </View>
+      <ProductsList />
+      <StatusBar style={ColorScheme === "dark" ? "light" : "dark"} />
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ products.js
@@ -0,0 +1,14 @@
+export const products = [
+    {
+        id: 1,
+        title: "Laptops",
+        price: 109.95,
+        description: "tu laptop preferida",
+        category: "informatica",
+        image: "https//fakestoreapi.com/img/71-3HiGNDUL._AC_SY879._SC._UX._SY._UY_.jpg",
+        rating: {
+            rate: 3.9,
+            count: 120,
+        },
+    },
+]
